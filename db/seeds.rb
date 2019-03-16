d6798b620fd438737b354c9d006e7eccbda10db2
“Brett <bretttewell@gmail.com>
2019-03-16 14:49:44 -0500

    ran seeds file to author db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+author = Author.create(name: "Stephen King", bio: "One of the most prolific writers in history.", genre_id: 1)
+author = Author.create(name: "Phillip K Dick", bio: "An American writer known for his work in science fiction.", genre_id: 3)
+author = Author.create(name: "H.P. Lovecraft", bio: "an American writer who achieved posthumous fame through his influential works of horror fiction.", genre_id: 1)
+author = Author.create(name: "Isaac Asimov", bio: "An American writer and professor of biochemistry at Boston University.", genre_id: 3)
+author = Author.create(name: "J.R.R. Tolkien", bio: "was an English writer, poet, philologist, and academic, widely regarded as the originator of classic high fantasy.", genre_id: 2)
+author = Author.create(name: "Piers Anthony", bio: "An English-American author in the science fiction and fantasy genres.", genre_id: 2)
+
+
+
